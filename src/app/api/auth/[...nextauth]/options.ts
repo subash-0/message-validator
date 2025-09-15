382513d22cb3ff9ebe1917b68309427a933dd2ec
subash-0 <subash67335@gmail.com>
2025-09-15 22:28:38 +0545

    Fet: added the final set up

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,7 +1,7 @@
 import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/modals/user.model";
-import CredentialsProvider from "next-auth/providers/credentials";
 import { comparePassword } from "@/helpers/bcrypt";
 import { redisClient } from "@/lib/redis";
 
@@ -14,86 +14,89 @@ export const authOptions: NextAuthOptions = {
         identifier: { label: "Email or Username", type: "text" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials: any ):Promise<any> {
+      async authorize(
+        credentials: Record<"identifier" | "password", string> | undefined
+      ): Promise<null | {
+        id:string,
+        _id: string | undefined;
+        email: string;
+        name: string;
+        username: string;
+        isVerified: boolean;
+        isAcceptingMessage: boolean;
+        sessionToken: string;
+      }> {
+        if (!credentials) return null;
+
         await dbConnect();
 
         const user = await UserModel.findOne({
           $or: [
-            { email: credentials?.identifier },
-            { username: credentials?.identifier },
+            { email: credentials.identifier },
+            { username: credentials.identifier },
           ],
         });
 
         if (!user) throw new Error("User not found!");
-        if (!user.isVerified)
-          throw new Error("Verify your account before login!");
+        if (!user.isVerified) throw new Error("Verify your account before login!");
 
-        const isMatch = await comparePassword(
-          credentials?.password ?? "",
-          user.password
-        );
-
+        const isMatch = await comparePassword(credentials.password, user.password);
         if (!isMatch) throw new Error("Invalid credentials!");
+
         const existingSession = await redisClient.get(`session:${user._id}`);
         if (existingSession) {
           throw new Error("You are already logged in on another device.");
         }
 
         const sessionToken = crypto.randomUUID();
-
         await redisClient.setex(
           `session:${user._id}`,
-          60 * 60 * 24 * 7, // TTL in seconds (7 days)
-          sessionToken // the value
+          60 * 60 * 24 * 7, // 7 days
+          sessionToken
         );
 
         return {
-          _id: user._id as string,
+          id:user.id,
+          _id: user._id?.toString(),
           email: user.email,
           name: user.name,
           username: user.username,
           isVerified: user.isVerified,
           isAcceptingMessage: user.isAcceptingMessage,
-          sessionToken
+          sessionToken,
         };
       },
     }),
   ],
   callbacks: {
     async jwt({ token, user }) {
-       if (user) {
-        token.sessionToken = user.sessionToken;
-      }
-      
       if (user) {
-        token._id = user._id?.toString();
+        token._id = user._id;
+        token.username = user.username;
         token.isVerified = user.isVerified;
         token.isAcceptingMessage = user.isAcceptingMessage;
-        token.username = user.username;
+        token.sessionToken = user.sessionToken;
         token.name = user.name;
-       
       }
 
-      if (token?._id && token?.sessionToken) {
+      if (token._id && token.sessionToken) {
         const activeSession = await redisClient.get(`session:${token._id}`);
-
         if (!activeSession) throw new Error("Session expired. Please login again.");
-
         if (activeSession !== token.sessionToken) {
           throw new Error("You have been logged out due to login from another device.");
         }
       }
-     
+
       return token;
     },
     async session({ session, token }) {
       if (token) {
-        session.user._id = token._id;
-        session.user.isVerified = token.isVerified;
-        session.user.isAcceptingMessage = token.isAcceptingMessage;
-        session.user.username = token.username;
-        session.user.sessionToken = token.sessionToken;
-        session.user.name = token.name;
+        session.user._id = token._id!;
+        session.user.username = token.username!;
+        session.user.isVerified = token.isVerified!;
+        session.user.isAcceptingMessage = token.isAcceptingMessage!;
+        session.user.sessionToken = token.sessionToken!;
+        session.user.name = token.name!;
       }
       return session;
     },
@@ -108,13 +111,11 @@ export const authOptions: NextAuthOptions = {
     strategy: "jwt",
   },
   events: {
-  async signOut({ token }) {
-    if (token?._id) {
-      await redisClient.del(`session:${token._id}`);
-     
-    }
+    async signOut({ token }) {
+      if (token?._id) {
+        await redisClient.del(`session:${token._id}`);
+      }
+    },
   },
-},
-
   secret: process.env.NEXT_AUTH_SCRETE as string,
 };
